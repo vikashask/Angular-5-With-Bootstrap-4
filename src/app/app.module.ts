1ed1394f93afc32bf90bfcccbfaf8a11a9a7f150
vikashraj144 <vikashraj144@gmail.com>
2018-05-01 11:13:52 +0530

    changed in app moule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,21 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EventListComponent } from './components/event-list/event-list.component';
+import { EventThumbnailComponent } from './components/event-thumbnail/event-thumbnail.component';
+import { EventDetailsComponent } from './components/event-details/event-details.component';
+import { NavComponent } from './nav/nav.component';
+import { EventService } from './components/shared/event.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,7 +34,7 @@ import { HomeComponent } from './home/home.component';
       }
     ])
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
