6e23d475f69b9c505e88203fdd3278f9aa0740d1
vikashraj144 <vikashraj144@gmail.com>
2018-05-02 19:34:55 +0530

    app module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+// added barrel
+
+import {
+  CreateEventComponent,
+  EventDetailsComponent,
+  EventThumbnailComponent,
+  EventListComponent,
+} from './components/index';
+
 import { HomeComponent } from './home/home.component';
-import { EventListComponent } from './components/event-list/event-list.component';
-import { EventThumbnailComponent } from './components/event-thumbnail/event-thumbnail.component';
-import { EventDetailsComponent } from './components/event-details/event-details.component';
+// import { EventListComponent } from './components/event-list/event-list.component';
+// import { EventThumbnailComponent } from './components/event-thumbnail/event-thumbnail.component';
+// import { EventDetailsComponent } from './components/event-details/event-details.component';
 import { NavComponent } from './nav/nav.component';
 import { EventService } from './components/shared/event.service';
-
+import { appRoutes } from './routes';
+// import { CreateEventComponent } from './components/create-event/create-event.component';
+import { Error404Component } from './components/shared/error/404.component';
+import { EventRouteActivator } from './components/event-details/event-route-activator';
+import { EventListResolver } from './components/shared/event-list-resolver.service';
 
 @NgModule({
   declarations: [
@@ -19,22 +33,27 @@ import { EventService } from './components/shared/event.service';
     EventThumbnailComponent,
     EventDetailsComponent,
     NavComponent,
+    CreateEventComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      }
-    ])
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [EventService],
+  providers: [EventService, EventRouteActivator,
+    EventListResolver,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  }
+  return true;
+}
