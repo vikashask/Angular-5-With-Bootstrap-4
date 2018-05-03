5bbc070d8096feac845ced26a54ddc1b6507d7e1
vikashraj144 <vikashraj144@gmail.com>
2018-05-03 20:39:08 +0530

    user module and route added

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { userRoutes } from './user.routes';
+import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(userRoutes)
+  ],
+  declarations: [
+    ProfileComponent,
+    LoginComponent
+  ],
+  providers: [
+
+  ]
+})
+export class UserModule { }
