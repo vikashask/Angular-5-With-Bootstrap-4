4915168ca2c19a0286893b022da1dc3677f1be17
vikashraj144 <vikashraj144@gmail.com>
2018-05-04 21:10:31 +0530

    index added

---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export * from './create-event/create-event.component';
+export * from './event-details/event-details.component';
+export * from './event-list/event-list.component';
+export * from './event-thumbnail/event-thumbnail.component';
