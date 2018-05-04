a1d063394d417c8bd7d5c3ce10ae45fffdd1d659
vikashraj144 <vikashraj144@gmail.com>
2018-05-04 21:12:31 +0530

    nav component added

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [`
   .nav.navbar-nav {font-size: 15px;}
   #searchForm {margin-right: 100px;}
-  @media (max-width: 1200px) {#searchForm {display:none}}
+  @media (max-width: 1200px) {#searchForm {display:none}},
+  li > a.active { color: #F97924; }
   `]
 })
 export class NavComponent implements OnInit {
